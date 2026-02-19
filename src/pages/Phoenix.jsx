96a9bdfaac64481e846f58571b57f1a1a3eb57e5
Bryan Gintoft <bpgintoft@gmail.com>
2026-02-19 05:50:22 +0000

    Editable clickable URL field

--- src/pages/Phoenix.jsx
+++ src/pages/Phoenix.jsx
@@ -64,7 +64,7 @@ export default function PhoenixPage() {
       </div>
 
       {phoenix && (
-        <div className="space-y-4 p-6 max-w-4xl mx-auto">
+        <div className="max-w-4xl mx-auto px-4 py-6 space-y-4">
           <SchoolProgramSection memberId={phoenix.id} memberName="Phoenix" />
           <FamilyMemberDetails memberId={phoenix.id} memberName="Phoenix" color="orange" />
         </div>
