3645b78035fc6bc8c7850295ba2ad1c0e69a6eff
Bryan Gintoft <bpgintoft@gmail.com>
2026-02-19 00:47:48 +0000

    File changes

--- src/pages/Phoenix.jsx
+++ src/pages/Phoenix.jsx
@@ -37,14 +37,12 @@ export default function PhoenixPage() {
         }
       `}</style>
       <div className="relative h-64 bg-gradient-to-r from-orange-500 to-orange-600 overflow-hidden phoenix-banner">
-        <div className="absolute right-0 bottom-0 z-10 h-60 w-auto">
-          <img 
-            src="https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/6990e4185e2b18f4d04a1ac8/bce10fc11_Phoenixpage.png"
-            alt="Phoenix"
-            className="h-full w-auto object-contain"
-            style={{ mixBlendMode: 'multiply' }}
-          />
-        </div>
+        <img 
+          src="https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/6990e4185e2b18f4d04a1ac8/bce10fc11_Phoenixpage.png"
+          alt="Phoenix"
+          className="absolute right-0 bottom-0 z-10 h-60 w-auto object-contain object-right-bottom"
+          style={{ mixBlendMode: 'multiply' }}
+        />
         <div className="relative z-10 h-full flex flex-col justify-end p-6 sm:p-8 pr-40 sm:pr-8">
           <motion.h1 
             initial={{ opacity: 0, y: 20 }}
