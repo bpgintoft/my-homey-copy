bbace79daa906f6c2560e7def144e21e2c1fbd0a
Bryan Gintoft <bpgintoft@gmail.com>
2026-02-22 03:07:41 +0000

    File changes

--- src/pages/Phoenix.jsx
+++ src/pages/Phoenix.jsx
@@ -80,7 +80,7 @@ export default function PhoenixPage() {
       </div>
 
       {phoenix && (
-        <div className="max-w-2xl mx-auto px-4 sm:px-6 py-6 space-y-4">
+        <div className="max-w-2xl mx-auto px-4 sm:px-6 pt-2 pb-6 space-y-4">
           <SchoolProgramSection memberId={phoenix.id} memberName="Phoenix" />
           <FamilyMemberDetails memberId={phoenix.id} memberName="Phoenix" color="orange" />
         </div>
