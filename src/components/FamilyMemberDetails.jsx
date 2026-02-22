49a78aa169c036a098267f7d1321e8b9ab6ae72a
Bryan Gintoft <bpgintoft@gmail.com>
2026-02-22 03:23:15 +0000

    File changes

--- src/components/FamilyMemberDetails.jsx
+++ src/components/FamilyMemberDetails.jsx
@@ -286,7 +286,7 @@ export default function FamilyMemberDetails({ memberId, memberName, color = 'blu
           </div>
         </div>
         <div 
-          className="cursor-pointer flex flex-col items-center text-center"
+          className="cursor-pointer flex flex-col items-center text-center ml-1"
           onClick={() => {
             const newDate = prompt('Enter license expiration date (MM/DD/YYYY):');
             if (newDate) {
