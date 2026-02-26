741d0351557be43f3229bbed967dba188149696b
Bryan Gintoft <bpgintoft@gmail.com>
2026-02-26 01:31:50 +0000

    File changes

--- src/components/FamilyCalendar.jsx
+++ src/components/FamilyCalendar.jsx
@@ -562,12 +562,7 @@ export default function FamilyCalendar({ activities }) {
         </Button>
       </div>
 
-      {/* Error message */}
-      {googleError && (
-        <div className="mb-4 p-3 bg-red-50 border border-red-200 rounded-lg text-sm text-red-600">
-          Error loading Google Calendar: {googleError.message}
-        </div>
-      )}
+
 
       {/* Day labels */}
       <div className="flex gap-2 mb-3 px-2">
