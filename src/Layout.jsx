8f70af02f4059586ea31195410e42c655302d10a
Bryan Gintoft <bpgintoft@gmail.com>
2026-03-03 00:26:33 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 import { Link } from 'react-router-dom';
 import { createPageUrl } from '@/utils';
 import { 
