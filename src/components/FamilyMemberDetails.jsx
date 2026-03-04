477b81c40538a3d9dbf281463593b72fc9acd0fa
Bryan Gintoft <bpgintoft@gmail.com>
2026-03-04 12:52:44 +0000

    File changes

--- src/components/FamilyMemberDetails.jsx
+++ src/components/FamilyMemberDetails.jsx
@@ -14,6 +14,7 @@ import { Label } from "@/components/ui/label";
 import { DragDropContext, Droppable, Draggable } from '@hello-pangea/dnd';
 import { createPortal } from 'react-dom';
 import SchoolProgramSection from './SchoolProgramSection';
+import HealthMedicalSection from './HealthMedicalSection';
 import LinkedMaintenancePanel from './house/LinkedMaintenancePanel';
 import CoAssignedChorePanel from './CoAssignedChorePanel';
 import RescheduleDialog from './house/RescheduleDialog';
