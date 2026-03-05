f40ec711c78659a8d0628b45bd1222354ca08703
Bryan Gintoft <bpgintoft@gmail.com>
2026-03-05 18:15:27 +0000

    File changes

--- src/pages/Kids.jsx
+++ src/pages/Kids.jsx
@@ -16,6 +16,7 @@ import { Plus, Calendar as CalendarIcon, MapPin, DollarSign, Clock, Sparkles, Us
 import { motion } from 'framer-motion';
 import { format, startOfMonth, endOfMonth, eachDayOfInterval, isSameMonth, isSameDay, parseISO } from 'date-fns';
 import FamilyCalendar from '../components/FamilyCalendar';
+import ImportantDatesTab from '../components/ImportantDatesTab';
 
 export default function Kids() {
   const navigate = useNavigate();
