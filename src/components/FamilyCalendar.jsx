7aea048aaa75729a6347ff64489f270c0dea0c8b
Bryan Gintoft <bpgintoft@gmail.com>
2026-03-06 13:22:22 +0000

    File changes

--- src/components/FamilyCalendar.jsx
+++ src/components/FamilyCalendar.jsx
@@ -209,6 +209,7 @@ export default function FamilyCalendar({ activities }) {
           title: eventData.summary,
           date: startDate,
           category: importantDateCategory || 'other',
+          custom_category: eventData.importantDateCustomCategory || '',
           description: eventData.description || '',
         });
       }
