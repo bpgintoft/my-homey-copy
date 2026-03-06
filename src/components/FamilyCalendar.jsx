8f0723bcdaee24cfcba80959754cc35341a8b907
Bryan Gintoft <bpgintoft@gmail.com>
2026-03-06 03:24:52 +0000

    File changes

--- src/components/FamilyCalendar.jsx
+++ src/components/FamilyCalendar.jsx
@@ -629,7 +629,7 @@ export default function FamilyCalendar({ activities }) {
   return (
     <div className="-mx-6 px-6">
       {/* Sticky header section */}
-      <div className="sticky z-20 pb-3 -mx-6 px-6" style={{ top: '104px', backgroundColor: '#F5F5F7' }}>
+      <div className="sticky top-16 lg:top-0 z-20 pb-3 -mx-6 px-6" style={{ backgroundColor: '#F5F5F7' }}>
       {/* Week navigation */}
       <div className="flex items-center justify-between gap-0.5 mb-3 pt-3">
         <Button
