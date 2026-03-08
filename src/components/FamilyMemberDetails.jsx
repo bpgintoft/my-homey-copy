063fc1fa439bb547b2231e0fde67ba369c80e3d5
Bryan Gintoft <bpgintoft@gmail.com>
2026-03-08 03:30:30 +0000

    File changes

--- src/components/FamilyMemberDetails.jsx
+++ src/components/FamilyMemberDetails.jsx
@@ -1024,7 +1024,7 @@ export default function FamilyMemberDetails({ memberId, memberName, color = 'blu
                       })}
                     </div>
                   </div>
-                ))}
+                ); })}
               </div>
             )}
 
