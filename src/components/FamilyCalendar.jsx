636863a0816b5a8df4cd7298aa413780c3f4812c
Bryan Gintoft <bpgintoft@gmail.com>
2026-03-10 23:04:59 +0000

    File changes

--- src/components/FamilyCalendar.jsx
+++ src/components/FamilyCalendar.jsx
@@ -917,7 +917,7 @@ export default function FamilyCalendar({ activities }) {
       )}
 
       {/* Events list grouped by day */}
-      <div className={`space-y-3 ${showMonthlyView ? 'hidden' : ''} pt-20`}>
+      <div className={`space-y-3 ${showMonthlyView ? 'hidden' : ''} pt-3`}>
         <AnimatePresence>
           {weekDays.map((day) => {
             const allDayActivities = getActivitiesForDay(day);
