03569b246edf1982b622f5be28f559f4f1207f17
Bryan Gintoft <bpgintoft@gmail.com>
2026-03-10 19:39:25 +0000

    File changes

--- src/components/MonthlyCalendar.jsx
+++ src/components/MonthlyCalendar.jsx
@@ -183,7 +183,7 @@ export default function MonthlyCalendar({ activities }) {
   return (
     <div className="-mx-6 px-6">
       {/* Month navigation */}
-      <div className="sticky top-0 z-20 pb-3 -mx-6 px-6 pt-3" style={{ backgroundColor: '#F5F5F7' }}>
+      <div ref={stickyHeaderRef} className="sticky top-0 z-20 pb-3 -mx-6 px-6 pt-3" style={{ backgroundColor: '#F5F5F7' }}>
         <div className="flex items-center justify-between mb-3">
           <Button variant="ghost" size="icon" className="h-6 w-6 p-0" onClick={() => setCurrentMonth(subMonths(currentMonth, 1))}>
             <ChevronLeft className="w-3.5 h-3.5" />
