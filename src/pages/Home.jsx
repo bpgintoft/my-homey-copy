552dbc0661b37425597bb515f1f9c714a30efb8f
Bryan Gintoft <bpgintoft@gmail.com>
2026-03-14 18:01:19 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -170,7 +170,7 @@ export default function Home() {
             <Link to={createPageUrl('Bryan')}>
               <Card className="border-0 shadow-lg bg-white hover:shadow-xl transition-shadow cursor-pointer">
                 <CardContent className="py-2 px-2 text-center">
-                  <div className="text-sm font-bold text-gray-900">{bryanMember.name}</div>
+                  <div className="text-xs font-bold text-gray-900">{bryanMember.name}</div>
                 </CardContent>
               </Card>
             </Link>
