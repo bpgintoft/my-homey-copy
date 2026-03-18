2dfa8704871d4526e7ec9fe000a609c2bb469b0f
Bryan Gintoft <bpgintoft@gmail.com>
2026-03-18 03:18:22 +0000

    File changes

--- src/components/RoundaboutGrid.jsx
+++ src/components/RoundaboutGrid.jsx
@@ -171,9 +171,9 @@ export default function RoundaboutGrid({ sections, imageUrls }) {
         {/* Central diamond button */}
         <Link to={createPageUrl('Decisions')}>
           <motion.div
-            initial={{ opacity: 0, scale: 0, rotate: 45 }}
-            animate={{ opacity: 1, scale: 1, rotate: 45 }}
-            transition={{ duration: 0.4, delay: 0.5 }}
+            initial={{ opacity: 0, scale: 0.95 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.4, delay: 0.4 }}
             className="hover:brightness-110 transition-all duration-300"
             style={{
               position: 'absolute',
