9492411e017552fa61b62ffb338c2a05baab8de5
Bryan Gintoft <bpgintoft@gmail.com>
2026-03-20 22:53:09 +0000

    File changes

--- src/pages/Family.jsx
+++ src/pages/Family.jsx
@@ -105,7 +105,7 @@ export default function Family() {
 
     return (
       <div className="space-y-4 mt-4">
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+        <div className="grid grid-cols-1 gap-4">
           <div>
             <Label>Name *</Label>
             <Input 
