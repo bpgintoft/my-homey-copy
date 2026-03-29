54d057976396f05c86e0c9a533cfc6c60d9ae61d
Bryan Gintoft <bpgintoft@gmail.com>
2026-03-29 23:18:58 +0000

    File changes

--- src/pages/Meals.jsx
+++ src/pages/Meals.jsx
@@ -764,7 +764,7 @@ export default function Meals() {
 
           <TabsContent value="meals" className="space-y-6">
             <h2 className="text-xl font-semibold text-gray-900">
-              {filteredMeals.length} Kid-Friendly Meals
+              {filteredMeals.length} Family Favorites
             </h2>
 
             <div className="space-y-4">
