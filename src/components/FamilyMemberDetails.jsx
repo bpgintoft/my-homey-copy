7e45dc5390804eba0dd0331a643530e1d4a3efec
Bryan Gintoft <bpgintoft@gmail.com>
2026-03-29 04:10:24 +0000

    File changes

--- src/components/FamilyMemberDetails.jsx
+++ src/components/FamilyMemberDetails.jsx
@@ -708,7 +708,7 @@ export default function FamilyMemberDetails({ memberId, memberName, color = 'blu
           const sectionIconMap = {
             chores: <img src="https://media.base44.com/images/public/6990e4185e2b18f4d04a1ac8/f073545eb_todolist.png" alt="To-Do List" className="w-7 h-7 flex-shrink-0 object-contain" />,
             schoolProgram: member?.person_type !== 'adult' ? <span className="text-lg flex-shrink-0">🎓</span> : <img src="https://media.base44.com/images/public/6990e4185e2b18f4d04a1ac8/25730819a_ChatGPTImageMar28202610_48_14PM.png" alt="Work" className="w-7 h-7 flex-shrink-0 object-contain" />,
-            links: <span className="text-lg flex-shrink-0">🔗</span>,
+            links: <img src="https://media.base44.com/images/public/6990e4185e2b18f4d04a1ac8/2338d2772_links.png" alt="Links" className="w-7 h-7 flex-shrink-0 object-contain" />,
             contacts: <img src="https://media.base44.com/images/public/6990e4185e2b18f4d04a1ac8/80a1d630f_contacts.png" alt="Contacts" className="w-7 h-7 flex-shrink-0 object-contain" />,
             notes: <span className="text-lg flex-shrink-0">💡</span>,
             milestones: <img src="https://media.base44.com/images/public/6990e4185e2b18f4d04a1ac8/c1a626e77_goals.png" alt="Goals" className="w-7 h-7 flex-shrink-0 object-contain" />,
